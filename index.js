edb1c8fd30a59cf265d2867be6681a2c6547470f
Monica Marziano <mmarziano1@gmail.com>
2017-12-01 10:14:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ function theBeatlesPlay (musicians, instruments){
   }
   return sayings;
 }
+
+function johnLennonFacts (facts) {
+  const facts = ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
+  var newFacts = [];
+  var i = 0;
+  while (i < facts.length) {
+    newFacts.push(facts(i)+"!!!")
+    i++;
+  }
+  return newFacts;
+}
